57001fcf3a6f7129d7ffe4ad5f050b1816852ef4
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2024-01-30 01:20:31 +0100

    Begin

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+export const App: React.FC = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Firebase Experiment</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
